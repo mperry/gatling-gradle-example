d6b23dd129282ef02dd45c7ce09aaccbe985fa86
Mark Perry <maperry78+github@gmail.com>
2015-09-14 09:24:17 +1000

    Add users and ramp

--- src/test/scala/com/github/mperry/gatling/BasicSimulation.scala
+++ src/test/scala/com/github/mperry/gatling/BasicSimulation.scala
@@ -1,27 +1,25 @@
 
-package com.github.mperry.gatling // 1
+package com.github.mperry.gatling
 
-import io.gatling.core.Predef._ // 2
+import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class BasicSimulation extends Simulation { // 3
+class BasicSimulation extends Simulation {
 
-  val httpConf = http // 4
-    .baseURL("http://localhost:8080/server") // 5
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+  val httpConf = http
+    .baseURL("http://localhost:8080/server")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("BasicSimulation") // 7
-    .exec(http("http://localhost:8080/server")  // 8
-    .get("/")) // 9
-    .pause(5) // 10
+  val scn = scenario("BasicSimulation")
+    .exec(http("main page")
+    .get("/"))
+    .pause(1)
 
-  setUp( // 11
-    scn.inject(atOnceUsers(1)) // 12
-  ).protocols(httpConf) // 13
+  setUp(scn.inject(rampUsers(10).over(3 seconds))).protocols(httpConf)
 
 }
